27db885275d9dca4d4e7cd6effd6821ef2ed3ca5
Kariqs <kariukibenard189@gmail.com>
2025-11-04 15:01:41 +0300

    Added the about page.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.mindhealthai',
-  appName: 'MindHealtAI',
+  appName: 'MindHealthAI',
   webDir: 'dist/emotional-support-chatbot/browser',
 
   plugins: {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { Signup } from './components/signup/signup';
 import { Login } from './components/login/login';
 import { authGuard } from './services/guards/auth-guard';
 import { Notfound } from './components/notfound/notfound';
+import { About } from './components/about/about';
 
 export const routes: Routes = [
   { path: '', component: Splash },
   { path: 'chat', component: Chat, canActivate: [authGuard] },
   { path: 'signup', component: Signup },
   { path: 'login', component: Login },
+  { path: 'about', component: About },
 
   { path: '**', component: Notfound },
 ];
